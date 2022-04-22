bfd116edf711d6452a58ca0040bf8b084f4934cb
tjerk-zeilstra <lala@pepr.nl>
2022-04-22 10:27:10 +0200

    basic chat

---
+++ Chat-service/Chat-service/Models/ChatMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Chat_service.Models
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public string? Message { get; set; }
+        public string? User { get; set; }
+        public string? ChatRoom { get; set; }
+    }
+}
